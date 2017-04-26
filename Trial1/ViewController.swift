841a9bdccb65db3963b62fd97b973f654d063270
mobilecoe <mobilecoe@mobilecoes-mac-mini.local>
2017-04-26 16:16:59 +0530

    Initial Commit

---
+++ Trial1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trial1
+//
+//  Created by mobilecoe on 26/04/17.
+//  Copyright © 2017 mobilecoe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
